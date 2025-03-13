14efd350b795f8582c496b71628943dc36f977cd
jasonkneo <jasonkneo@gmail.com>
2025-03-13 13:45:57 +1000

    Add files via upload

--- web_zoning_checker.py
+++ web_zoning_checker.py
@@ -15,9 +15,7 @@ OVERLAY_URLS = {
     "Traditional Building Character": "https://services2.arcgis.com/dEKgZETqwmDAh1rP/arcgis/rest/services/Traditional_building_character_overlay/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson",
     "Bushfire": "https://services2.arcgis.com/dEKgZETqwmDAh1rP/arcgis/rest/services/Bushfire_overlay/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson",
     "Heritage": "HERITAGE_OVERLAY_GEOJSON_URL",
-    "Pre-1911 Buildings": "https://services2.arcgis.com/dEKgZETqwmDAh1rP/arcgis/rest/services/Pre1911_building_overlay/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson",
-    }
-GEOJSON_PATH = "zoning_data.geojson"
+    
 OVERLAY_PATHS = {key: f"{key.lower().replace(' ', '_')}_overlay.geojson" for key in OVERLAY_URLS.keys()}
 
 def geocode_address(address):
